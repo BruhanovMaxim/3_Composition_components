c196a6e99f4d910bd7a165f1824d97fc65beca22
Maxim <hot8max@gmail.com>
2022-04-06 21:08:15 +0300

    Done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,44 +1,42 @@
-import React, { useState } from "react";
+import React from 'react'
+import { child } from './../untils'
+
 const Counter = (props) => {
-    const {value}= props
-    const formatValue = () => {
-        return value === 0 ? "empty" : value;
-    };
-    const getBageClasses = () => {
-        let classes = "badge m-2 ";
-        classes += value === 0 ? "bg-warning" : "bg-primary";
-        return classes;
-    };
+  let { id, value } = props
 
-    const handleIncrement = () => {
-        console.log('handleIncrement')
-        // setValue((prevState) => prevState + 1);
-    };
-    const handleDecrement = () => {
-        console.log('handleDecrement')
-        // setValue((prevState) => prevState - 1);
-    };
+  const formatCount = () => {
+    return value <= 0 ? 'empty' : value
+  }
 
-    return (
-        <div>
-            <span>{props.name}</span>
-            <span className={getBageClasses()}>{formatValue()}</span>
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleIncrement}
-            >
-                +
-            </button>
-            <button
-                className='btn btn-primary btn-sm m-2'
-                onClick={handleDecrement}
-            >
-                -
-            </button>
-            <button className="btn btn-danger btn-sm m-2" onClick={()=>props.onDelete(props.id)}>Delete</button>
-            
-        </div>
-    );
-};
+  const getBageClasses = () => {
+    let classes = ' badge m-2 '
+    classes += value <= 0 ? 'bg-warning' : 'bg-primary'
+    return classes
+  }
 
-export default Counter;
+  return (
+    <>
+      <div>
+        <span>{props.name}</span>
+        <span className={getBageClasses()}>{formatCount()}</span>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={props.onIncrement.bind(null, id)}
+          children={child('+')}
+        />
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={props.onDecrement.bind(null, id)}
+          children={child('-')}
+        />
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={props.onDelete.bind(null, id)}
+          children={child('Удалить')}
+        />
+      </div>
+    </>
+  )
+}
+
+export default Counter
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,49 +1,66 @@
-import React, {useState} from "react"
-import Counter from "./counter"
+import React, { useState } from 'react'
+import Counter from './counter'
+import { child } from './../untils'
 
-const CountersList = () => {
-    const initialState = [
-        { id: 0, value: 0, name: 'Ненужная вещь', price:'200'},
-        { id: 1, value: 4, name: 'Ложка'},
-        { id: 2, value: 0, name: 'Вилка'},
-        { id: 3, value: 0, name: 'Тарелка'},
-        { id: 4, value: 0, name: 'Набор минималиста'}
-    ]
-    const [counters, setCounters] = useState(initialState)
+const CounterList = () => {
+  const initialState = [
+    { id: 0, value: 0, name: 'Ненужная вещь' },
+    { id: 1, value: 0, name: 'Нужная вещь)' },
+    { id: 2, value: 0, name: 'Тарелка' },
+    { id: 3, value: 0, name: 'Вилка' },
+    { id: 4, value: 0, name: 'Ложка' },
+    { id: 5, value: 0, name: 'Набор минималиста' },
+  ]
+  const [counters, setCounters] = useState(initialState)
 
-    const handleDelete = (id)=>{
-        const newCounters = counters.filter(c=>c.id!==id)
-        setCounters(newCounters)
-    }
-
-    const handleReset = ()=>{
-        setCounters(initialState)
-        console.log('handleReset')
-    }
-
-        const handleUpdate = ()=>{
-        const updateState = [
-        { id: 0, value: 1, name: 'Ненужная вещь', price:'200'},
-        { id: 1, value: 2, name: 'Ложка'},
-        { id: 2, value: 3, name: 'Вилка'},
-        { id: 3, value: 4, name: 'Тарелка'},
-        { id: 4, value: 0, name: 'Набор минималиста'}
-    ]      
-        setCounters(updateState)
-    }
-
-    return (
-        <>
-            {counters.map((count)=>(
-            <Counter
-            key={count.id}
-            onDelete={handleDelete}
-            {...count}
-             />
-            ))}
-            <button className="btn btn-primary btn-sm m2" onClick={handleReset}>Сброс</button>
-            <button className="btn btn-primary btn-sm m2" onClick={handleUpdate}>Обновить состояние</button>
-        </>
+  const handleIncrement = (id) => {
+    setCounters((counters) =>
+      counters.filter((counter) => {
+        return counter.id === id ? (counter.value += 1) : counter
+      })
     )
+  }
+
+  const handleDecrement = (id) => {
+    setCounters((counters) =>
+      counters.filter((counter) => {
+        if (counter.id === id && counter.value > 0) {
+          counter.value -= 1
+          return counter
+        }
+        return counter
+      })
+    )
+  }
+
+  const handleDelete = (id) => {
+    setCounters((counters) => counters.filter((counter) => counter.id !== id))
+  }
+
+  const handleReset = () => {
+    setCounters((prevState) => (prevState = initialState))
+  }
+
+  return (
+    <>
+      {counters.map((count) => {
+        return (
+          <Counter
+            key={count.id}
+            {...count}
+            onDelete={handleDelete}
+            onIncrement={handleIncrement}
+            onDecrement={handleDecrement}
+          />
+        )
+      })}
+      <button
+        className="btn btn-success btn-sm m-2"
+        onClick={handleReset}
+        children={child('Сброс')}
+      />
+    </>
+  )
 }
-export default CountersList;
+
+export default CounterList
